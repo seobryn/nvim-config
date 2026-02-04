e7a0539d9167cd5c8d52ac6254a76f79893ba168
Jose Joya <ing.jose.joya@gmail.com>
2026-02-04 12:40:26 -0500

    Update config to remove relative numbers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "d", "_d", opts)
+vim.keymap.set("n", "dd", '"_dd', opts) -- Delete line to black hole
+vim.keymap.set("v", "d", '"_d', opts) -- Visual delete to black hole
